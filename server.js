559a17e57d82dea7da4d19c38fe25561249409ef
Ashishshill <aks3ashish@gmail.com>
2022-09-28 11:38:04 +0600

    change to env valiables

--- server.js
+++ server.js
@@ -22,6 +22,8 @@ const PORT = 8000;
 const username = process.env.DB_USERNAME;
 const password = process.env.DB_PASSWORD;
 
-Connection(username, password);
+const URL = process.env.MONGO_URL || `mongodb+srv://${username}:${password}@cluster0.kcp45ny.mongodb.net/?retryWrites=true&w=majority`;
+
+Connection(URL);
 
 app.listen(PORT, () => console.log(`Server is running successfully on PORT ${PORT}`));
